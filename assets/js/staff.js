944bd008b892d6c6cff9448954a9c6307780f0c3
Henry Campos <henry.campos@galileo.edu>
2019-04-29 21:43:30 -0600

    New update!
    v0.2

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,59 +1,91 @@
 var staff = [
     {
+        name : 'Diego Velásquez',
+        information : 'Google Developer Expert de Flutter.',
+        image : 'diegovel.jpg',
+        email : '@diegoveloper',
+        gde: '1',
+        company: 'Aeyrium'
+    },
+    {
+        name : 'Diego Ramírez',
+        information : 'Software Engineer en Wizeline y coorganizador del GDG Android Titlan en Ciudad de México.',
+        image : 'diegoram.jpg',
+        email : '-',
+        company: 'Wizeline'
+    },
+    {
+        name : 'Diego Coy',
+        information : 'Desarrollador web apasionado por compartir conocimiento.',
+        image : 'diegocoy.png',
+        email : '-',
+        gde : '1',
+        company: 'Wizeline'
+    },
+    {
         name : 'Adrián Catalán',
         information : 'Innovation Lab leader and Google Developer Expert.',
         image : 'adrian.jpg',
         job : 'boss',
-        email : 'adriancatalan@galileo.edu'
+        email : 'adriancatalan@galileo.edu',
+        gde: '1',
+        company: 'Universidad Galileo'
     },
     {
         name : 'Luis Jimenez',
         information : 'Interested in computer vision, hardware and education.',
         image : 'luis.jpg',
         job : 'projects-leader',
-        email : 'luiscu@galileo.edu'
+        email : 'luiscu@galileo.edu',
+        company: 'Universidad Galileo'
     },
     {
         name : 'Evelyn Cruz',
         information : 'Labs and workshops instructor with a huge interest in Biomedic field.',
         image : 'eve.png',
         job : 'workshops-leader',
-        email : 'evecruz@galileo.edu'
+        email : 'evecruz@galileo.edu',
+        company: 'Universidad Galileo'
     },
     {
         name : 'Sofi Cárdenas',
         information : 'Currently working on workshops provided by the lab to school students.',
         image : 'sofi.png',
         job : 'workshops-assistant',
-        email : 'ascardenas@galileo.edu'
+        email : 'ascardenas@galileo.edu',
+        company: 'Universidad Galileo'
     },
     {
         name: 'Holly Milián',
         information: 'Graphic Designer, Illustrator and Amateur Photographer ★ Love cartoons and draw silly things ★',
         image: 'holly.jpeg',
         job: 'graphic-designer',
-        email: 'ikuzuhunter@gmail.com'
+        email: 'ikuzuhunter@gmail.com',
+        company: 'Universidad Galileo'
     },
     {
         name : 'Henry Campos',
         information : 'Systems engineer, web developer/designer, AI interested and math lover.',
         image : 'henry.png',
         job : 'projects-assistant',
-        email : 'henry.campos@galileo.edu'
+        email : 'henry.campos@galileo.edu',
+        company: 'Universidad Galileo'
     },
     {
         name : 'Fernando López',
         information : 'Working on workshops with main interest on mechanical design, love to make things work.',
         image : 'fernando.png',
         job : 'workshops-assistant',
-        email : 'fernando.lopez@galileo.edu'
+        email : 'fernando.lopez@galileo.edu',
+        company: 'Universidad Galileo'
     },
     {
         name : 'Jerry Palacios',
         information : 'Working working working working working.',
         image : 'jerry.png',
         job : 'projects-assistant',
-        email : 'jerry.palacios@galileo.edu'
+        email : 'jerry.palacios@galileo.edu',
+        company: 'Universidad Galileo'
     }
 ];
 
