3a857d3a71691cbd2865b1294640a1bf961e1676
ascardenas <ascardenas@galileo.edu>
2019-04-30 13:12:14 -0600

    Update staff.js
    Se borraron los integrantes del lab para solo dejar a los ponentes.

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -46,62 +46,6 @@ var staff = [
         gde: '1',
         company: 'Universidad Galileo'
     },
-    {
-        name : 'Luis Jimenez',
-        information : 'Interested in computer vision, hardware and education.',
-        image : 'luis.jpg',
-        job : 'projects-leader',
-        email : 'luiscu@galileo.edu',
-        company: 'Universidad Galileo'
-    },
-    {
-        name : 'Evelyn Cruz',
-        information : 'Labs and workshops instructor with a huge interest in Biomedic field.',
-        image : 'eve.png',
-        job : 'workshops-leader',
-        email : 'evecruz@galileo.edu',
-        company: 'Universidad Galileo'
-    },
-    {
-        name : 'Sofi Cárdenas',
-        information : 'Currently working on workshops provided by the lab to school students.',
-        image : 'sofi.png',
-        job : 'workshops-assistant',
-        email : 'ascardenas@galileo.edu',
-        company: 'Universidad Galileo'
-    },
-    {
-        name: 'Holly Milián',
-        information: 'Graphic Designer, Illustrator and Amateur Photographer ★ Love cartoons and draw silly things ★',
-        image: 'holly.jpeg',
-        job: 'graphic-designer',
-        email: 'ikuzuhunter@gmail.com',
-        company: 'Universidad Galileo'
-    },
-    {
-        name : 'Henry Campos',
-        information : 'Systems engineer, web developer/designer, AI interested and math lover.',
-        image : 'henry.png',
-        job : 'projects-assistant',
-        email : 'henry.campos@galileo.edu',
-        company: 'Universidad Galileo'
-    },
-    {
-        name : 'Fernando López',
-        information : 'Working on workshops with main interest on mechanical design, love to make things work.',
-        image : 'fernando.png',
-        job : 'workshops-assistant',
-        email : 'fernando.lopez@galileo.edu',
-        company: 'Universidad Galileo'
-    },
-    {
-        name : 'Jerry Palacios',
-        information : 'Working working working working working.',
-        image : 'jerry.png',
-        job : 'projects-assistant',
-        email : 'jerry.palacios@galileo.edu',
-        company: 'Universidad Galileo'
-    }
 ];
 
 //Studied Electronical Engineering at Universidad Galileo. Currently working on workshops provided by the lab to school students in order to motivate and guide them towards learning and enjoying to work with different technologies. Her interests include AR, VR, and automation solutions.
