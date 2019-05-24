fb9b37d1ad1c40c4a32be30f5daa66d49b0e4d24
ascardenas <ascardenas@galileo.edu>
2019-05-24 10:44:34 -0600

    Update staff.js
    agregue algunos nacionales

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,8 +1,65 @@
 var staff = [
        {
+        name : 'Andrés Castellanos',
+        information : 'Ingeniero en Sistemas, miembro del Laboratorio Turing de Universidad Galileo, apasionado por aprender tecnologías state-of-the-art y en crear nuevas cosas',
+        image : 'castellanos.jpeg',
+        nationality: 'guatemala',
+        email : '-',
+        company: 'Universidad Galileo'
+    },
+       {
+        name : 'Herberth Guillermo Obregón Espini',
+        information : 'Soy CTO de Conectate Today, plataforma que ayuda a la difusión y generación de empleo, soy apasionado a las tecnologías de Google.',
+        image : 'Herberth.png',
+        nationality: 'guatemala',
+        email : '-',
+        company: 'Conectate Today'
+    },
+       {
+        name : 'Ceci Castillo',
+        information : 'Ceci es Ingeniera en sistemas, Se dedica al desarrollo de aplicaciones móviles en Xoom,  ha trabajado en aplicaciones de Android e iOS. Ella coorganiza el grupo de usuarios de desarrolladores de Google en Guatemala y el evento Women Tech Maker.',
+        image : 'ceci castillo.jpg',
+        nationality: 'guatemala',
+        email : '-',
+        company: 'Xoom'
+    },
+       {
+        name : 'César Calderón',
+        information : 'Consultor de seguridad / Pentester en la empresa Devel Security S.A. Cuento con varios años de experiencia en el sector de la ciberseguridad enfocado a la auditorías de aplicaciones móviles, web e infraestructura y en este último año me he dedicado a realizar múltiples retos de seguridad informática para diferentes eventos.',
+        image : 'Cesar calderon.jpeg',
+        nationality: 'guatemala',
+        email : '-',
+        company: 'Devel Security, S.A.'
+    },
+       {
+        name : 'Víctor Orozco',
+        information : 'Víctor cuenta con un master en Ciencias de la Computación como ex becario OEA. ganador del premio Duke’s Choice Award 2016, Java Champion y Oracle Groundbreaker Ambassador.',
+        image : 'Victor Orozco.jpg',
+        nationality: 'guatemala',
+        email : '-',
+        company: '-'
+    },
+          {
+        name : 'Andrea Lara',
+        information : 'La Magister Andrea Lara es Ingeniera en electrónica graduada de Universidad Galileo, obtuvo una maestría en ingeniería biomédica de la Universidad de Lubeck, Alemania y actualmente es PhD Candidate de Ingeniería Biomédica en la Universidad Tecnológica de Graz, Austria. ',
+        image : 'Andrea Lara.jpg',
+        nationality: 'guatemala',
+        email : '-',
+        company: 'Universidad Galileo'
+    },
+       {
+        name : 'Jorge Cajas',
+        information : 'Desarrollador Java / Android / React con más de 6 años de experiencia. Miembro de Guate-JUG y ganador del Oracle Duke Choice Award en 2016.',
+        image : 'foto_cuadrada.jpg',
+        nationality: 'guatemala',
+        email : '-',
+        company: '-'
+    },
+       {
         name : 'Hansy Schmitt',
         information : 'Desarrollador Mobile especializado en Flutter y Android, manager de la comunidad cloud de Lima/Peru (GCDC Lima) , con interes en hacking etico, ingenieria reversa, domotica y Voice interfaces.',
         image : 'hansy.jpg',
+        nationality: 'peru',
         email : '-',
         company: '-'
     },
@@ -10,6 +67,7 @@ var staff = [
         name : 'Carlo Huamán',
         information : 'Android / Chatbot / Assistant Developer, GDE, Software Engineer in Avantica & Cyclist.',
         image : 'CarlosRenzo.png',
+        nationality: 'peru',
         email : '-',
         company: 'Avantica',
         gde: '1'
@@ -18,6 +76,7 @@ var staff = [
         name : 'Juan Ñahui',
         information : 'Apasionado por la tecnología y con la programación, actualmente me especializo en Inteligencia Artificial, soy parte de Hack Space Perú y también estudié Ingeniería Mecatrónica.',
         image : 'ñahui.jpeg',
+        nationality: 'peru',
         email : '@juanybabas',
         company: '-'
     },
@@ -25,6 +84,7 @@ var staff = [
         name : 'Katherine Chauca',
         information : 'G Suite Certified Deployment Specialist, Product Owner y organizadora de las comunidades GDG Lima, Google Cloud Developer Community Lima y Agile Perú.',
         image : 'KatherineChauca.jpg',
+        nationality: 'peru',
         email : '-',
         company: '-'
     },
@@ -33,6 +93,7 @@ var staff = [
         information : 'Google Developer Expert para Google Cloud Platform, co-founder y CTO de Metix y colaborador en el GDG Cloud MX.',
         image : 'Pablovillanuevafar.jpg',
         gde: '1',
+        nationality: 'mexico',
         email : '-',
         company: 'Metix'
     },
@@ -41,6 +102,7 @@ var staff = [
         information : 'Google Developer Expert en Estrategia de Producto, Directora de Operaciones de Makesense en México y Climate Reality Leader.',
         image : 'KenzaZouaoui.jpeg',
         gde: '1',
+        nationality: 'mexico',
         email : '-',
         company: 'Makesense'
     },
@@ -48,6 +110,7 @@ var staff = [
         name : 'Isabel Palomar',
         information : 'Co-organizadora de las comunidades de GDG Guadalajara GDG Guadalajara, Tech Women Community, WWCode Ciudad Guzmán y JUG PA.',
         image : 'IsabelPalomar.jpg',
+        nationality: 'mexico',
         email : '-',
         company: '-'
     },
@@ -56,6 +119,7 @@ var staff = [
         information : 'GDE de Cloud, Python Developer en Globant y parte de las comunidades GDG Bogotá y GCDC Bogotá.',
         image : 'Adrianamoya.jpeg',
         gde: '1',
+        nationality: 'colombia',
         email : '-',
         company: 'Globant'
     },
@@ -64,6 +128,7 @@ var staff = [
         information : 'Google Developer Expert de Flutter.',
         image : 'diegovel.jpg',
         email : '@diegoveloper',
+        nationality: 'peru',
         gde: '1',
         company: 'Aeyrium'
     },
@@ -71,6 +136,7 @@ var staff = [
         name : 'Diego Ramírez',
         information : 'Software Engineer en Wizeline y coorganizador del GDG Android Titlan en Ciudad de México.',
         image : 'diegoram.jpg',
+        nationality: 'mexico',
         email : '-',
         company: 'Wizeline'
     },
@@ -79,6 +145,7 @@ var staff = [
         information : 'Desarrollador web apasionado por compartir conocimiento.',
         image : 'diegocoy.png',
         email : '-',
+        nationality: 'colombia',
         gde : '1',
         company: '-'
     },
