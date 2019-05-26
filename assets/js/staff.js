9355eb4bb5039589f5b3b76b13ae4cd9bc069d52
Henry Campos <henry.campos@galileo.edu>
2019-05-26 10:37:26 -0600

    Font size fixes

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,183 +1,191 @@
 var staff = [
-        {
-        name : 'Yeffri Salazar',
-        information : 'Desarrollador de sistemas para internet de las cosas , emprendedor, pocaster, streamer , divulgador',
-        image : 'yefrisalazar.jpeg',
+    {
+        name: 'Henry Campos',
+        information: 'Estudiante de ingeniería en sistemas, miembro del Laboratorio de Innovación de Universidad Galileo, desarrollador/diseñador web, interesado en AI y amante de las matemáticas.',
+        image: 'henry.jpg',
+        email: 'henry.campos@galileo.edu',
+        company: 'Universidad Galileo',
+        nationality: 'guatemala'
+    },
+    {
+        name: 'Yeffri Salazar',
+        information: 'Desarrollador de sistemas para internet de las cosas , emprendedor, pocaster, streamer , divulgador',
+        image: 'yefrisalazar.jpeg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: '-'
     },
-       {
-        name : 'Norman Vicente',
-        information : 'Estudiante universitario, back-end, apasionado por el desarrollo.',
-        image : 'norman1.jpg',
+    {
+        name: 'Norman Vicente',
+        information: 'Estudiante universitario, back-end, apasionado por el desarrollo.',
+        image: 'norman1.jpg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: '-'
     },
-       {
-        name : 'Walter Corado',
-        information : 'Ingeniero en Sistemas dedicado al desarrollo de Aplicaciones Web empleando metodologías de Desarrollo Ágil, ha trabajado en diferentes proyectos de desarrollo en diferentes empresas y es encargado de la comunidad Node.js en Guatemala.',
-        image : 'Walter_Cordero.JPG',
+    {
+        name: 'Walter Corado',
+        information: 'Ingeniero en Sistemas dedicado al desarrollo de Aplicaciones Web empleando metodologías de Desarrollo Ágil, ha trabajado en diferentes proyectos de desarrollo en diferentes empresas y es encargado de la comunidad Node.js en Guatemala.',
+        image: 'Walter_Cordero.JPG',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: '-'
     },
-       {
-        name : 'Andrés Castellanos',
-        information : 'Ingeniero en Sistemas, miembro del Laboratorio Turing de Universidad Galileo, apasionado por aprender tecnologías state-of-the-art y en crear nuevas cosas',
-        image : 'castellanos.jpeg',
+    {
+        name: 'Andrés Castellanos',
+        information: 'Ingeniero en Sistemas, miembro del Laboratorio Turing de Universidad Galileo, apasionado por aprender tecnologías state-of-the-art y en crear nuevas cosas',
+        image: 'castellanos.jpeg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: 'Universidad Galileo'
     },
-       {
-        name : 'Herberth Obregón ',
-        information : 'Soy CTO de Conectate Today, plataforma que ayuda a la difusión y generación de empleo, soy apasionado a las tecnologías de Google.',
-        image : 'Herberth.png',
+    {
+        name: 'Herberth Obregón ',
+        information: 'Soy CTO de Conectate Today, plataforma que ayuda a la difusión y generación de empleo, soy apasionado a las tecnologías de Google.',
+        image: 'Herberth.png',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: 'Conectate Today'
     },
-       {
-        name : 'Ceci Castillo',
-        information : 'Ceci es Ingeniera en sistemas, Se dedica al desarrollo de aplicaciones móviles en Xoom,  ha trabajado en aplicaciones de Android e iOS. Ella coorganiza el grupo de usuarios de desarrolladores de Google en Guatemala y el evento Women Tech Maker.',
-        image : 'ceci castillo.jpg',
+    {
+        name: 'Ceci Castillo',
+        information: 'Ceci es Ingeniera en sistemas, Se dedica al desarrollo de aplicaciones móviles en Xoom,  ha trabajado en aplicaciones de Android e iOS. Ella coorganiza el grupo de usuarios de desarrolladores de Google en Guatemala y el evento Women Tech Maker.',
+        image: 'ceci castillo.jpg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: 'Xoom'
     },
-       {
-        name : 'César Calderón',
-        information : 'Consultor de seguridad / Pentester en la empresa Devel Security S.A. Cuento con varios años de experiencia en el sector de la ciberseguridad enfocado a la auditorías de aplicaciones móviles, web e infraestructura.',
-        image : 'Cesar calderon.jpeg',
+    {
+        name: 'César Calderón',
+        information: 'Consultor de seguridad / Pentester en la empresa Devel Security S.A. Cuento con varios años de experiencia en el sector de la ciberseguridad enfocado a la auditorías de aplicaciones móviles, web e infraestructura.',
+        image: 'Cesar calderon.jpeg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: 'Devel Security, S.A.'
     },
-       {
-        name : 'Víctor Orozco',
-        information : 'Víctor cuenta con un master en Ciencias de la Computación como ex becario OEA. ganador del premio Duke’s Choice Award 2016, Java Champion y Oracle Groundbreaker Ambassador.',
-        image : 'Victor Orozco.jpg',
+    {
+        name: 'Víctor Orozco',
+        information: 'Víctor cuenta con un master en Ciencias de la Computación como ex becario OEA. ganador del premio Duke’s Choice Award 2016, Java Champion y Oracle Groundbreaker Ambassador.',
+        image: 'Victor Orozco.jpg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: '-'
     },
-          {
-        name : 'Andrea Lara',
-        information : 'La Magister Andrea Lara es Ingeniera en electrónica graduada de Universidad Galileo, obtuvo una maestría en ingeniería biomédica de la Universidad de Lubeck, Alemania y actualmente es PhD Candidate de Ingeniería Biomédica en la Universidad Tecnológica de Graz, Austria. ',
-        image : 'Andrea Lara.jpg',
+    {
+        name: 'Andrea Lara',
+        information: 'La Magister Andrea Lara es Ingeniera en electrónica graduada de Universidad Galileo, obtuvo una maestría en ingeniería biomédica de la Universidad de Lubeck, Alemania y actualmente es PhD Candidate de Ingeniería Biomédica en la Universidad Tecnológica de Graz, Austria. ',
+        image: 'Andrea Lara.jpg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: 'Universidad Galileo'
     },
-       {
-        name : 'Jorge Cajas',
-        information : 'Desarrollador Java / Android / React con más de 6 años de experiencia. Miembro de Guate-JUG y ganador del Oracle Duke Choice Award en 2016.',
-        image : 'foto_cuadrada.jpg',
+    {
+        name: 'Jorge Cajas',
+        information: 'Desarrollador Java / Android / React con más de 6 años de experiencia. Miembro de Guate-JUG y ganador del Oracle Duke Choice Award en 2016.',
+        image: 'foto_cuadrada.jpg',
         nationality: 'guatemala',
-        email : '-',
+        email: '-',
         company: '-'
     },
-       {
-        name : 'Hansy Schmitt',
-        information : 'Desarrollador Mobile especializado en Flutter y Android, manager de la comunidad cloud de Lima/Peru (GCDC Lima) , con interes en hacking etico, ingenieria reversa, domotica y Voice interfaces.',
-        image : 'hansy.jpg',
+    {
+        name: 'Hansy Schmitt',
+        information: 'Desarrollador Mobile especializado en Flutter y Android, manager de la comunidad cloud de Lima/Peru (GCDC Lima) , con interes en hacking etico, ingenieria reversa, domotica y Voice interfaces.',
+        image: 'hansy.jpg',
         nationality: 'peru',
-        email : '-',
+        email: '-',
         company: '-'
     },
-      {
-        name : 'Carlo Huamán',
-        information : 'Android / Chatbot / Assistant Developer, GDE, Software Engineer in Avantica & Cyclist.',
-        image : 'CarlosRenzo.png',
+    {
+        name: 'Carlo Huamán',
+        information: 'Android / Chatbot / Assistant Developer, GDE, Software Engineer in Avantica & Cyclist.',
+        image: 'CarlosRenzo.png',
         nationality: 'peru',
-        email : '-',
+        email: '-',
         company: 'Avantica',
         gde: '1'
     },
-     {
-        name : 'Juan Ñahui',
-        information : 'Apasionado por la tecnología y con la programación, actualmente me especializo en Inteligencia Artificial, soy parte de Hack Space Perú y también estudié Ingeniería Mecatrónica.',
-        image : 'ñahui.jpeg',
+    {
+        name: 'Juan Ñahui',
+        information: 'Apasionado por la tecnología y con la programación, actualmente me especializo en Inteligencia Artificial, soy parte de Hack Space Perú y también estudié Ingeniería Mecatrónica.',
+        image: 'ñahui.jpeg',
         nationality: 'peru',
-        email : '@juanybabas',
+        email: '@juanybabas',
         company: '-'
     },
     {
-        name : 'Katherine Chauca',
-        information : 'G Suite Certified Deployment Specialist, Product Owner y organizadora de las comunidades GDG Lima, Google Cloud Developer Community Lima y Agile Perú.',
-        image : 'KatherineChauca.jpg',
+        name: 'Katherine Chauca',
+        information: 'G Suite Certified Deployment Specialist, Product Owner y organizadora de las comunidades GDG Lima, Google Cloud Developer Community Lima y Agile Perú.',
+        image: 'KatherineChauca.jpg',
         nationality: 'peru',
-        email : '-',
+        email: '-',
         company: '-'
     },
     {
-        name : 'Pablo Villanueva',
-        information : 'Google Developer Expert para Google Cloud Platform, co-founder y CTO de Metix y colaborador en el GDG Cloud MX.',
-        image : 'Pablovillanuevafar.jpg',
+        name: 'Pablo Villanueva',
+        information: 'Google Developer Expert para Google Cloud Platform, co-founder y CTO de Metix y colaborador en el GDG Cloud MX.',
+        image: 'Pablovillanuevafar.jpg',
         gde: '1',
         nationality: 'mexico',
-        email : '-',
+        email: '-',
         company: 'Metix'
     },
     {
-        name : 'Kenza Zouaoui',
-        information : 'Google Developer Expert en Estrategia de Producto, Directora de Operaciones de Makesense en México y Climate Reality Leader.',
-        image : 'KenzaZouaoui.jpeg',
+        name: 'Kenza Zouaoui',
+        information: 'Google Developer Expert en Estrategia de Producto, Directora de Operaciones de Makesense en México y Climate Reality Leader.',
+        image: 'KenzaZouaoui.jpeg',
         gde: '1',
         nationality: 'mexico',
-        email : '-',
+        email: '-',
         company: 'Makesense'
     },
     {
-        name : 'Isabel Palomar',
-        information : 'Co-organizadora de las comunidades de GDG Guadalajara GDG Guadalajara, Tech Women Community, WWCode Ciudad Guzmán y JUG PA.',
-        image : 'IsabelPalomar.jpg',
+        name: 'Isabel Palomar',
+        information: 'Co-organizadora de las comunidades de GDG Guadalajara GDG Guadalajara, Tech Women Community, WWCode Ciudad Guzmán y JUG PA.',
+        image: 'IsabelPalomar.jpg',
         nationality: 'mexico',
-        email : '-',
+        email: '-',
         company: '-'
     },
     {
-        name : 'Adriana Moya',
-        information : 'GDE de Cloud, Python Developer en Globant y parte de las comunidades GDG Bogotá y GCDC Bogotá.',
-        image : 'Adrianamoya.jpeg',
+        name: 'Adriana Moya',
+        information: 'GDE de Cloud, Python Developer en Globant y parte de las comunidades GDG Bogotá y GCDC Bogotá.',
+        image: 'Adrianamoya.jpeg',
         gde: '1',
         nationality: 'colombia',
-        email : '-',
+        email: '-',
         company: 'Globant'
     },
     {
-        name : 'Diego Velásquez',
-        information : 'Google Developer Expert de Flutter.',
-        image : 'diegovel.jpg',
-        email : '@diegoveloper',
+        name: 'Diego Velásquez',
+        information: 'Google Developer Expert de Flutter.',
+        image: 'diegovel.jpg',
+        email: '@diegoveloper',
         nationality: 'peru',
         gde: '1',
         company: 'Aeyrium'
     },
     {
-        name : 'Diego Ramírez',
-        information : 'Software Engineer en Wizeline y coorganizador del GDG Android Titlan en Ciudad de México.',
-        image : 'diegoram.jpg',
+        name: 'Diego Ramírez',
+        information: 'Software Engineer en Wizeline y coorganizador del GDG Android Titlan en Ciudad de México.',
+        image: 'diegoram.jpg',
         nationality: 'mexico',
-        email : '-',
+        email: '-',
         company: 'Wizeline'
     },
     {
-        name : 'Diego Coy',
-        information : 'Google Developer Expert de Web y desarrollador web apasionado por compartir conocimiento.',
-        image : 'diegocoy.png',
-        email : '-',
+        name: 'Diego Coy',
+        information: 'Google Developer Expert de Web y desarrollador web apasionado por compartir conocimiento.',
+        image: 'diegocoy.png',
+        email: '-',
         nationality: 'colombia',
-        gde : '1',
+        gde: '1',
         company: '-'
     },
     {
-        name : 'Adrián Catalán',
-        information : 'GDE de Android, IoT y Firebase, mentor de launchpad, sprint master. Organizador de GCDC Guatemala y director del Innovation Lab de GalileoU.',
-        image : 'adrian.jpg',
-        email : 'adriancatalan@galileo.edu',
+        name: 'Adrián Catalán',
+        information: 'GDE de Android, IoT y Firebase, mentor de launchpad, sprint master. Organizador de GCDC Guatemala y director del Innovation Lab de GalileoU.',
+        image: 'adrian.jpg',
+        email: 'adriancatalan@galileo.edu',
         gde: '1',
         company: 'Universidad Galileo',
         nationality: 'guatemala'
