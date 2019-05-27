ec48acca8732c5e774c3af48efc78ef4b2034b47
Luis Jimenez <luisrrjjcc@galileo.edu>
2019-05-27 10:14:07 -0600

    Charlas de jueves en la mañana agregadas a events

--- assets/js/events.js
+++ assets/js/events.js
@@ -32,5 +32,89 @@ var events = [
         subjects: [
             'Inteligencia Artificial'
         ]
-    }
+    },
+    	{
+		name : 'Hacking 101: Técnicas básicas de ataque y defensa',
+		description : 'Veremos cuáles son los vectores de ataques comunes hacia los sistemas informáticos, y de qué manera podemos prevenirlos.',
+		speaker : 'Jorge Cajas',
+		day : '30',
+		time : '10:30',
+		place : 'Salón 2',
+		subjects : [
+			'Seguridad Informática'
+		]
+	},
+	{
+		name : 'Primeros pasos con Flutter',
+		description : 'Aprende sobre como empezar a trabajar con Flutter, el framework de desarrollo multiplataforma de Google.',
+		speaker : 'Henry Campos',
+		day : '30',
+		time : '10:30',
+		place : 'Salón 3',
+		subjects : [
+			'Flutter'
+		]
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'Katherine Chauca',
+		day : '30',
+		time : '11:30',
+		place : 'Salón 2',
+		subjects : []
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'Hansy Schmitt',
+		day : '30',
+		time : '11:30',
+		place : 'Salón 3',
+		subjects : [
+			'Flutter'
+		]
+	},
+	{
+		name : 'Hey Google! Conociendo el asistente virtual de Google',
+		description : 'TBA',
+		speaker : 'Carlo Huamán',
+		day : '30',
+		time : '12:30',
+		place : 'Salón 1',
+		subjects : [
+			'Google Assistant'
+		]
+	},
+	{
+		name : 'Firebase en el mundo del Internet de las Cosas',
+		description : 'TBA',
+		speaker : 'Yeffri Salazar',
+		day : '30',
+		time : '12:15',
+		place : 'Salón 2',
+		subjects : [
+			'IoT', 'Firebase'
+		]
+	},
+	{
+		name : 'Building a Custom Machine Learning Model on Android',
+		description : 'Aprenderás como crear tu aplicación de Android que puede detectar y etiquetar imágenes con un modelo de Tensorflow Lite en ML Kit for Firebase.',
+		speaker : 'Isabel Palomar',
+		day : '30',
+		time : '18:30',
+		place : 'Salón 1',
+		subjects : [
+			'Inteligencia Artificial', 'Tensorflow', 'Firebase'
+		]
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'Katherine Chauca',
+		day : '30',
+		time : '19:30',
+		place : 'Salón 1',
+		subjects : []
+	}
 ];
