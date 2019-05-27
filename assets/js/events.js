324ac02e4e990493963d767ff7c9b9524ae5703e
Luis Jimenez <luisrrjjcc@galileo.edu>
2019-05-27 14:36:43 -0600

    Todas las conferencias hasta el momento.

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,39 +1,52 @@
 var events = [
-    {
-        name : 'El Futuro y la Biomédica',
-        description : 'Dónde estamos y hacia dónde vamos en la tecnología médica y cómo esta ayudará a tener una mejor calidad de vida.',
-        speaker : 'Andrea Lara',
-        day: '30',
-        time: '09:45',
-        place: 'Salón 1',
-        subjects: [
-            'Biomédica'
-        ]
-    },
-    {
-        name : 'Integrating Google Dialogflow and Messenger',
-        description : 'Dialogflow es la herramienta de Natural Language Processing de Google, veremos cómo hacer la integración Messenger.',
-        speaker : 'Norman Vicente',
-        day: '30',
-        time: '09:45',
-        place: 'Salón 3',
-        subjects: [
-            'DialogFlow',
-            'Messenger'
-        ]
-    },
-    {
-        name : 'Qué es el aprendizaje automático y cómo puedo usarlo para que ella o él me amen',
-        description : 'Enseñando a programar a niños descubrió nuevas formas de enseñar. Curso básico sobre inteligencia artificial animada.',
-        speaker : 'Juan Ñahui',
-        day: '30',
-        time: '10:30',
-        place: 'Salón 1',
-        subjects: [
-            'Inteligencia Artificial'
-        ]
-    },
-    	{
+	{
+		name : 'El Futuro y la Biomédica',
+		description : 'Dónde estamos y hacia dónde vamos en la tecnología médica y cómo esta ayudará a tener una mejor calidad de vida.',
+		speaker : 'Andrea Lara',
+		day: '30',
+		time: '09:45',
+		place: 'Salón 1',
+		subjects: [
+			'Biomédica'
+		],
+		tba : '0'
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'TBA',
+		day : '30',
+		time : '09:45',
+		place : 'Salón 2',
+		subjects : [],
+		tba : '1'
+	},
+	{
+		name : 'Integrating Google Dialogflow and Messenger',
+		description : 'Dialogflow es la herramienta de Natural Language Processing de Google, veremos cómo hacer la integración Messenger.',
+		speaker : 'Norman Vicente',
+		day: '30',
+		time: '09:45',
+		place: 'Salón 3',
+		subjects: [
+			'DialogFlow', 'Messenger'
+		],
+		tba : '0'
+	},
+
+	{
+		name : 'Qué es el aprendizaje automático y cómo puedo usarlo para que ella o él me amen',
+		description : 'Enseñando a programar a niños descubrió nuevas formas de enseñar. Curso básico sobre inteligencia artificial animada.',
+		speaker : 'Juan Ñahui',
+		day: '30',
+		time: '10:30',
+		place: 'Salón 1',
+		subjects: [
+			'Inteligencia Artificial'
+		],
+		tba : '0'
+	},
+		{
 		name : 'Hacking 101: Técnicas básicas de ataque y defensa',
 		description : 'Veremos cuáles son los vectores de ataques comunes hacia los sistemas informáticos, y de qué manera podemos prevenirlos.',
 		speaker : 'Jorge Cajas',
@@ -42,7 +55,8 @@ var events = [
 		place : 'Salón 2',
 		subjects : [
 			'Seguridad Informática'
-		]
+		],
+		tba : '0'
 	},
 	{
 		name : 'Primeros pasos con Flutter',
@@ -53,28 +67,43 @@ var events = [
 		place : 'Salón 3',
 		subjects : [
 			'Flutter'
-		]
+		],
+		tba : '0'
+	},
+
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'TBA',
+		day : '30',
+		time : '11:45',
+		place : 'Salón 1',
+		subjects : [],
+		tba : '1'
 	},
 	{
 		name : 'TBA',
 		description : 'TBA',
 		speaker : 'Katherine Chauca',
 		day : '30',
-		time : '11:30',
+		time : '11:45',
 		place : 'Salón 2',
-		subjects : []
+		subjects : [],
+		tba : '0'
 	},
 	{
 		name : 'TBA',
 		description : 'TBA',
 		speaker : 'Hansy Schmitt',
 		day : '30',
-		time : '11:30',
+		time : '11:45',
 		place : 'Salón 3',
 		subjects : [
 			'Flutter'
-		]
+		],
+		tba : '0'
 	},
+
 	{
 		name : 'Hey Google! Conociendo el asistente virtual de Google',
 		description : 'TBA',
@@ -84,19 +113,32 @@ var events = [
 		place : 'Salón 1',
 		subjects : [
 			'Google Assistant'
-		]
+		],
+		tba : '0'
 	},
 	{
 		name : 'Firebase en el mundo del Internet de las Cosas',
 		description : 'TBA',
 		speaker : 'Yeffri Salazar',
 		day : '30',
-		time : '12:15',
+		time : '12:30',
 		place : 'Salón 2',
 		subjects : [
 			'IoT', 'Firebase'
-		]
+		],
+		tba : '0'
 	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'TBA',
+		day : '30',
+		time : '12:30',
+		place : 'Salón 3',
+		subjects : [],
+		tba : '1'
+	},
+
 	{
 		name : 'Building a Custom Machine Learning Model on Android',
 		description : 'Aprenderás como crear tu aplicación de Android que puede detectar y etiquetar imágenes con un modelo de Tensorflow Lite en ML Kit for Firebase.',
@@ -106,29 +148,10 @@ var events = [
 		place : 'Salón 1',
 		subjects : [
 			'Inteligencia Artificial', 'Tensorflow', 'Firebase'
-		]
+		],
+		tba : '0'
 	},
-	{
-		name : 'TBA',
-		description : 'TBA',
-		speaker : 'Katherine Chauca',
-		day : '30',
-		time : '19:30',
-		place : 'Salón 1',
-		subjects : []
-	},
-	{
-		name : 'Diseño responsivo en Flutter',
-		description : 'TBA',
-		speaker : 'Hansy Schmitt',
-		day : '30',
-		time : '20:10',
-		place : 'Salón 1',
-		subjects : [
-			'Flutter'
-		]
-	},
-	{
+		{
 		name : 'Progressive Web Apps, the new trend to create Apps',
 		description : 'TBA',
 		speaker : 'Walter Cordero',
@@ -137,27 +160,8 @@ var events = [
 		place : 'Salón 2',
 		subjects : [
 			'PWA'
-		]
-	},
-	{
-		name : '¿Por qué Flutter sería una buena elección para su empresa?',
-		description : 'Actualmente tenemos el dilema de las aplicaciones, no sabemos si elegir aplicaciones nativas o híbridas. Breve introducción a Flutter y cómo podría ser la opción correcta para su empresa.',
-		speaker : 'Diego Velásquez',
-		day : '30',
-		time : '19:30',
-		place : 'Salón 2',
-		subjects : [
-			'Flutter'
-		]
-	},
-	{
-		name : 'TBA',
-		description : 'TBA',
-		speaker : 'Víctor Orozco',
-		day : '30',
-		time : '20:10',
-		place : 'Salón 2',
-		subjects : []
+		],
+		tba : '0'
 	},
 	{
 		name : 'Severless Architecture for our Actions powered by Google',
@@ -168,7 +172,32 @@ var events = [
 		place : 'Salón 3',
 		subjects : [
 			'Serverless', 'Google Cloud'
-		]
+		],
+		tba : '0'
+	},
+
+
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'Katherine Chauca',
+		day : '30',
+		time : '19:30',
+		place : 'Salón 1',
+		subjects : [],
+		tba : '0'
+	},
+		{
+		name : '¿Por qué Flutter sería una buena elección para su empresa?',
+		description : 'Actualmente tenemos el dilema de las aplicaciones, no sabemos si elegir aplicaciones nativas o híbridas. Breve introducción a Flutter y cómo podría ser la opción correcta para su empresa.',
+		speaker : 'Diego Velásquez',
+		day : '30',
+		time : '19:30',
+		place : 'Salón 2',
+		subjects : [
+			'Flutter'
+		],
+		tba : '0'
 	},
 	{
 		name : 'Corrutinas en Android',
@@ -179,7 +208,31 @@ var events = [
 		place : 'Salón 3',
 		subjects : [
 			'Kotlin'
-		]
+		],
+		tba : '0'
+	},
+
+	{
+		name : 'Diseño responsivo en Flutter',
+		description : 'TBA',
+		speaker : 'Hansy Schmitt',
+		day : '30',
+		time : '20:10',
+		place : 'Salón 1',
+		subjects : [
+			'Flutter'
+		],
+		tba : '0'
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'Víctor Orozco',
+		day : '30',
+		time : '20:10',
+		place : 'Salón 2',
+		subjects : [],
+		tba : '0'
 	},
 	{
 		name : 'Conectate Today: Google Full-Stack',
@@ -188,25 +241,136 @@ var events = [
 		day : '30',
 		time : '20:10',
 		place : 'Salón 3',
-		subjects : []
+		subjects : [],
+		tba : '0'
 	},
-		{
+
+	{
+		name : 'Design Thinking - metodologías y prácticas para innovar',
+		description : 'Taller introductorio y creativo para entender Design Thinking y descubrir herramientas para identificar retos y desarrollar soluciones innovadoras',
+		speaker : 'Kenza Zouaoui',
+		day : '31',
+		time : '09:00',
+		place : 'Salón 1',
+		subjects : [
+			'Design Thinking'
+		],
+		tba : '0'
+	},
+	{
+		name : 'Construyendo un satélite a lo Maker',
+		description : '¿Se pueden construir satélites con componentes comunes y corrientes?',
+		speaker : 'José Bagur',
+		day : '31',
+		time : '9:00',
+		place : 'Salón 2',
+		subjects : [
+			'Maker'
+		],
+		tba : '0'
+	},
+	{
+		name : 'Resolución de problemas a través de la Ingeniería',
+		description : 'TBA',
+		speaker : 'Walter Cordero',
+		day : '31',
+		time : '9:00',
+		place : 'Salón 3',
+		subjects : [],
+		tba : '0'
+	},
+
+	{
+		name : 'Design Thinking (continuación de taller)',
+		description : 'Taller introductorio y creativo para entender Design Thinking y descubrir herramientas para identificar retos y desarrollar soluciones innovadoras',
+		speaker : 'Kenza Zouaoui',
+		day : '31',
+		time : '09:45',
+		place : 'Salón 1',
+		subjects : [
+			'Design Thinking'
+		],
+		tba : '0'
+	},
+	{
+		name : 'Inteligencia Artificial para Android',
+		description : 'TBA',
+		speaker : 'Isabel Palomar',
+		day : '31',
+		time : '09:45',
+		place : 'Salón 2',
+		subjects : [
+			'Inteligencia Artificial', 'Android'
+		],
+		tba : '0'
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'Evelin Juarez',
+		day : '31',
+		time : '09:45',
+		place : 'Salón 3',
+		subjects : [
+			'Inteligencia Artificial'
+		],
+		tba : '0'
+	},
+
+	{
 		name : 'TBA',
 		description : 'TBA',
 		speaker : 'Pablo Villanueva',
 		day : '31',
 		time : '10:30',
 		place : 'Salón 1',
-		subjects : []
+		subjects : [],
+		tba : '0'
 	},
 	{
 		name : 'TBA',
 		description : 'TBA',
-		speaker : 'Isabel Palomar',
+		speaker : 'Jorge Cajas',
 		day : '31',
-		time : '09:45',
+		time : '10:30',
 		place : 'Salón 2',
-		subjects : []
+		subjects : [],
+		tba : '0'
+	},
+	{
+		name : 'RISC-V',
+		description : 'TBA',
+		speaker : 'Andrés Castellanos',
+		day : '31',
+		time : '10:30',
+		place : 'Salón 3',
+		subjects : [
+			'Microprocesadores'
+		],
+		tba : '0'
+	},
+
+	{
+		name : 'La web cumplió 30 años, se casó, tuvo hijos y no te diste cuenta',
+		description : '',
+		speaker : 'Diego Coy',
+		day : '31',
+		time : '11:45',
+		place : 'Salón 1',
+		subjects : [
+			'Web'
+		],
+		tba : '0'
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'TBA',
+		day : '31',
+		time : '11:45',
+		place : 'Salón 2',
+		subjects : [],
+		tba : '0'
 	},
 	{
 		name : 'TBA',
@@ -215,7 +379,56 @@ var events = [
 		day : '31',
 		time : '11:45',
 		place : 'Salón 3',
-		subjects : []
+		subjects : [],
+		tba : '0'
+	},
+
+	{
+		name : 'Como hackear al mundo',
+		description : 'TBA',
+		speaker : 'Diego Ramírez',
+		day : '31',
+		time : '12:30',
+		place : 'Salón 1',
+		subjects : [],
+		tba : '0'
+	},
+	{
+		name : 'Deep Learning',
+		description : 'Aplicaciones de deep learning, el estado del arte y algunos cursos que pueden llevar para que puedan empezar en este tema.',
+		speaker : 'Andrés Castellanos',
+		day : '31',
+		time : '12:30',
+		place : 'Salón 2',
+		subjects : [
+			'Inteligencia Artificial'
+		],
+		tba : '0'
+	},
+	{
+		name : 'Predicción de series de tiempo con redes neuronales artificiales',
+		description : 'TBA',
+		speaker : 'Víctor Peñaloza',
+		day : '31',
+		time : '12:30',
+		place : 'Salón 3',
+		subjects : [
+			'Inteligencia Artificial'
+		],
+		tba : '0'
+	},
+
+	{
+		name : 'Creando aplicaciones sin servidores con Google Cloud Functions',
+		description : 'Vamos a conocer un servicio de Google Cloud Platform que nos permite realizar tareas con funciones de backend alojadas sobre Google Cloud.',
+		speaker : 'Adriana Moya',
+		day : '31',
+		time : '18:30',
+		place : 'Salón 1',
+		subjects : [
+			'Google Cloud', 'Serverless'
+		],
+		tba : '0'
 	},
 	{
 		name : 'AutoML',
@@ -226,19 +439,22 @@ var events = [
 		place : 'Salón 2',
 		subjects : [
 			'Inteligencia Artificial'
-		]
+		],
+		tba : '0'
 	},
 	{
-		name : 'Creando aplicaciones sin servidores con Google Cloud Functions',
-		description : 'Vamos a conocer un servicio de Google Cloud Platform que nos permite realizar tareas con funciones de backend alojadas sobre Google Cloud.',
-		speaker : 'Adriana Moya',
-		day : '31',
-		time : '18:30',
-		place : 'Salón 1',
-		subjects : [
-			'Google Cloud', 'Serverless'
-		]
+		name : 'Qué es el aprendizaje automático y cómo puedo usarlo para que ella o él me amen',
+		description : 'Enseñando a programar a niños descubrió nuevas formas de enseñar. Curso básico sobre inteligencia artificial animada.',
+		speaker : 'Juan Ñahui',
+		day: '31',
+		time: '18:30',
+		place: 'Salón 3',
+		subjects: [
+			'Inteligencia Artificial'
+		],
+		tba = '0'
 	},
+
 	{
 		name : 'Design Thinking - metodologías y prácticas para innovar',
 		description : 'Taller introductorio y creativo para entender Design Thinking y descubrir herramientas para identificar retos y desarrollar soluciones innovadoras',
@@ -248,17 +464,66 @@ var events = [
 		place : 'Salón 1',
 		subjects : [
 			'Design Thinking'
-		]
+		],
+		tba : '0'
 	},
 	{
-		name : 'Design Thinking - metodologías y prácticas para innovar',
-		description : 'Taller introductorio y creativo para entender Design Thinking y descubrir herramientas para identificar retos y desarrollar soluciones innovadoras',
-		speaker : 'Kenza Zouaoui',
+		name : 'Automatizando el proceso de release de Android con Fastlane',
+		description : 'Fastlane permite automatizaar el proceso de prueba y lanzamiento en iOS y Android. Veremos tareas como firmar códidgo, generar capturas y administrar metadatos.',
+		speaker : 'Ceci Castillo',
 		day : '31',
-		time : '09:00',
+		time : '19:30',
+		place : 'Salón 2',
+		subjects : [
+			'Android'
+		],
+		tba : '0'
+	},
+	{
+		name : 'Buenas prácticas en la implementqación de proyectos en Google Cloud Platform',
+		description : 'La implementación de buenas prácticas nos permite tener una mejor visibilidad y sacarle el provecho a los recursos disponibles',
+		speaker : 'Yolanda López',
+		day : '31',
+		time : '19:30',
+		place : 'Salón 3',
+		subjects : [
+			'Google Cloud'
+		],
+		tba : '0'
+	},
+	
+	{
+		name : 'Flutter para desarrolladores',
+		description : 'TBA',
+		speaker : 'Diego Velásquez',
+		day : '31',
+		time : '20:10',
 		place : 'Salón 1',
 		subjects : [
-			'Design Thinking'
-		]
+			'Flutter'
+		],
+		tba : '0'
+	},
+	{
+		name : 'Revelando los super poderes secretos de la web',
+		description : 'TBA',
+		speaker : 'Diego Coy',
+		day : '31',
+		time : '20:10',
+		place : 'Salón 2',
+		subjects : [
+			'Web'
+		],
+		tba : '0'
+	},
+	{
+		name : 'TBA',
+		description : 'TBA',
+		speaker : 'César Calderón',
+		day : '31',
+		time : '20:10',
+		place : 'Salón 3',
+		subjects : [],
+		tba : '0'
 	}
 ];
