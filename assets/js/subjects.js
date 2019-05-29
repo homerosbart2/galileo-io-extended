0bba5394e7c2aa5d57e5c86e2ea788ea7635c240
Henry Campos <henry.campos@galileo.edu>
2019-05-29 13:55:16 -0600

    Color to subjects added

---
+++ assets/js/subjects.js
@@ -0,0 +1,22 @@
+subjects = {
+    'Flutter': '54C5F8',
+    'Firebase': 'FFA712',
+    'Tensorflow': 'FFA712',
+    'Biomédica': '4285f4',
+    'Messenger': '0084FF',
+    'DialogFlow': 'EF6C00',
+    'Seguridad Informática': '25FE14',
+    'App Engine': '478AF9',
+    'Innovación': '00ADEF',
+    'Google Cloud': 'E94235',
+    'Google Apps Script': 'E94235',
+    'Google Assistant': '4285f4',
+    'G Suite': '4285f4',
+    //'Inteligencia Artificial': '9E7AC0',
+    'IoT': 'FF8000',
+    'PWA': '5A0FC8',
+    'Kotlin': '517CDE',
+    'Android': '34A853',
+    'Web': 'EE6535',
+    'Maker': 'EA1C24'
+};
